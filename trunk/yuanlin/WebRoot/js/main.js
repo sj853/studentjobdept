92962241560d5fd5d81390008c17cd8f2bd1fabf
shangjing853 <shangjing853@08760d2c-66bc-82de-6139-7ced615aa540>
2010-05-11 09:32:32 +0000

    页面之间的转换搞好了·· 一点点小效果··  真的只用1个页面啊···太锤子了吧 ···

--- trunk/yuanlin/WebRoot/js/main.js
+++ trunk/yuanlin/WebRoot/js/main.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 	
-$("#container").corner("20px");
 
 
 $('#nav a')
@@ -14,5 +13,17 @@ $('#nav a')
 		$(this).stop().animate(
 			{backgroundPosition:"(0 0)"}, 
 			{duration:300})
-		})
+		});
+$("#nav a").each(function(index){
+	$(this).click(function(){
+	$("div.showContent").hide();
+	
+	$("#mainContent div").eq(index).animate({
+		opacity:'show'
+	},3000).addClass("showContent");
+	
+	});
+});
+		
+$("#container").corner("20px");
 });
